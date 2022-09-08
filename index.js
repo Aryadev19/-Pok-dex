bb28f227b82e0b895041d817eaeb78fd0af1fa60
Piyush <piyusharyaprakash12345@gmail.com>
2022-09-08 20:23:10 +0530

    add views folder using path

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ const __filename = fileURLToPath(import.meta.url);
 
 const __dirname = path.dirname(__filename);
 
+app.set('views', path.join(__dirname, 'views'));
+
 app.get("/", (req, res) => {
   res.render("index");
 });

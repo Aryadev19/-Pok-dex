5ae980cdca1ecccc90a167c1e54d1ac54dadda66
Piyush <piyusharyaprakash12345@gmail.com>
2022-09-08 20:35:02 +0530

    fix the css not loading problem

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.set('views', path.join(__dirname, 'views'));
+
 app.use(express.static(__dirname + "/"));
 
 app.get("/", (req, res) => {
